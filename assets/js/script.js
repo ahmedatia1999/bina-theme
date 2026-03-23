01f6a85d9cc44ea19d5625c63ef8c542c65bc4a7
AhmedAtia <aatia069@gmail.com>
2026-03-23 15:44:36 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,29 +41,5 @@ $(document).ready(function(){
         $(".aside-menu").removeClass('active');
     });
    
-    /* Owl Carousel */
-    
-    $("#subscriptions-slider").owlCarousel({
-        loop: true,
-        margin: 20,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                margin: 10,
-            },
-            992: {
-                items: 1,
-            },
-            1199: {
-                items: 3,
-            }
-        },
-        dots: false,
-        nav: true,
-        rtl: rtl,
-        autoplay: false,
-        navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>']
-    });
     
 });
